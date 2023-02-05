e883346e68e14063f56abfeb1ccba2e466bc0d97
keremkiyak <110934008+keremkiyak@users.noreply.github.com>
2023-02-05 13:52:00 +0300

    Add files via upload

---
+++ optionalWithSwift.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+//optional olan bir değişkene nil atanabilir veya değer de atanabilir ama optional olmayan bir değişkene nil atanamaz.
+var name : String?
+var secondName : String? = "mehmet"
+print(name)
+print(secondName)
+
