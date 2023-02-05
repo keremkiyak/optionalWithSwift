f9f684de4a63e399c80b2cbfb9e2678b3d4099ff
keremkiyak <110934008+keremkiyak@users.noreply.github.com>
2023-02-05 14:20:31 +0300

    Add files via upload

---
+++ forceUnwrapping.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+//burda optional olan bir değişkeni force unwrapping ile normal değişkene dönüştürdük.
+var secondName : String? = "mehmet"
+var newName = secondName!
+print(newName)
+
